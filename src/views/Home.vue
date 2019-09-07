4c8f00089548a8d278a98bd26822d2d3ad00d16c
WS <183158862@qq.com>
2019-09-07 23:39:01 +0800

    new

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <!-- 子路由显示区 -->
+        <router-view />
+        <ul class="footer">
+            <li><router-link :to="{name: 'list'}">新闻列表</router-link></li>
+            <li><router-link :to="{name: 'user'}">个人中心</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped lang="scss">
+.footer{
+    position:fixed;
+    width: 100%;
+    height:60px;
+    line-height: 60px;
+    left:0;
+    bottom:0;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <form action="" v-if="!isReg">
+            <p>
+                用户名：<input type="text" v-model="name">
+            </p>
+            <p>
+                密码：<input type="password" v-model="password">
+            </p>
+            <button type="button" @click="login()">登录</button>
+            <button type="button" @click="reg()">注册</button>
+        </form>
+        <form action="" v-else>
+            <p>
+                用户名：<input type="text" v-model="name">
+            </p>
+            <p>
+                密码：<input type="password" v-model="password">
+            </p>
+            <p>
+                再次输入密码：<input type="password" v-model="repeat">
+            </p>
+            <button type="button" @click="addUser()">确定</button>
+            <button type="button" @click="cancel()">取消</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data () {
+            return {
+                isReg: false,
+                name: '',
+                password: '',
+                repeat: ''
+            }
+        },
+        methods: {
+            login () {
+                if (localStorage.getItem('name') === this.name && localStorage.getItem('password') === this.password) {
+                    this.$router.push('/home/list');  // 点击登录，路由进入home/list
+                    this.name = '';
+                    this.password = '';
+                } else {
+                    alert("输入的用户名或密码错误");
+                }
+            },
+            reg () {
+                this.isReg = true;
+            },
+            cancel () {
+                this.isReg = false;
+            },
+            addUser () {
+                if (this.password === this.repeat) {
+                    localStorage.setItem('name', this.name);
+                    localStorage.setItem('password', this.password);
+                    this.name = '';
+                    this.password = '';
+                    this.isReg = false;
+                } else {
+                    alert("两次密码输入不一致");
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
